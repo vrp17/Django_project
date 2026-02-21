dd44b45b00d2f52293a153dcf9acc6b15577d6ad
vrp17 <vedantrpatil16@gmail.com>
2026-02-22 01:00:26 +0530

    Applied UrlConfig Method for view

--- dj1/dj1/blog/views.py
+++ dj1/dj1/blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the blog Home Page")
+
+def about(request):
+    a = 50+19
+    return HttpResponse(f"Addition is :{a}")
--- dj1/dj1/dj1/urls.py
+++ dj1/dj1/dj1/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path, include
+# from blog import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('blog/', views.home, name='home'),
+    # path('blog/', views.home, name='home'),
+    path('', include('blog.urls'))
 ]
